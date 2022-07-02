ee410ab4dedb55f7c8e26b77341f884322d988a4
Israel David <77063876+israel1281@users.noreply.github.com>
2022-07-01 17:19:03 -0700

    add product details page

---
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,284 @@
+import { FC, memo } from "react";
+
+import { ArrowLeftIcon } from "./ArrowLeftIcon";
+import { Ellipse14Icon } from "./Ellipse14Icon";
+import { Ellipse26Icon } from "./Ellipse26Icon";
+import classes from "./ProductDetails.module.css";
+
+interface Props {
+  className?: string;
+  classes?: {
+    toyotaCamry7?: string;
+    _20000?: string;
+    negotiable?: string;
+    locationLagosNG?: string;
+    description?: string;
+    aboutSeller?: string;
+    automaticTransition4PlugsFirst?: string;
+    unsplashZbZ_EJJ4To?: string;
+    rectangle200?: string;
+    rectangle201?: string;
+    rectangle202?: string;
+    rectangle204?: string;
+    rectangle203?: string;
+    ellipse26?: string;
+    certifiedCarDealer?: string;
+    lagosNG?: string;
+    seunOlulade?: string;
+    rectangle2042?: string;
+    viewProfile?: string;
+    similarInOutlet?: string;
+    frame11?: string;
+    unsplashOUBjd22gF6w?: string;
+    unsplashP7tai9P7HS?: string;
+    unsplashO6yH_yAc2Ws?: string;
+    toyotaCamry72?: string;
+    washingMachine?: string;
+    _2BedroomFlat?: string;
+    _200002?: string;
+    _5000?: string;
+    _30000PerAnnum?: string;
+    lagosNigeria?: string;
+    abujaNigeria?: string;
+    alakiaIbadan?: string;
+    images1?: string;
+    rectangle208?: string;
+    _2GEDA11?: string;
+    rectangle199?: string;
+    rectangle195?: string;
+    messageSeller?: string;
+    details?: string;
+    ellipse14?: string;
+    arrowLeft?: string;
+  };
+}
+export const ProductDetails: FC<Props> = memo(function ProductDetails(
+  props = {}
+) {
+  return (
+    <div className={`${classes.root} ${props.className || ""}`}>
+      <div
+        className={`${classes.toyotaCamry7} ${
+          props.classes?.toyotaCamry7 || ""
+        }`}
+      >
+        Toyota Camry 07
+      </div>
+      <div className={`${classes._20000} ${props.classes?._20000 || ""}`}>
+        #2,000,000
+      </div>
+      <div
+        className={`${classes.negotiable} ${props.classes?.negotiable || ""}`}
+      >
+        Negotiable
+      </div>
+      <div
+        className={`${classes.locationLagosNG} ${
+          props.classes?.locationLagosNG || ""
+        }`}
+      >
+        Location : Lagos NG
+      </div>
+      <div
+        className={`${classes.description} ${props.classes?.description || ""}`}
+      >
+        Description
+      </div>
+      <div
+        className={`${classes.aboutSeller} ${props.classes?.aboutSeller || ""}`}
+      >
+        About seller
+      </div>
+      <div
+        className={`${classes.automaticTransition4PlugsFirst} ${
+          props.classes?.automaticTransition4PlugsFirst || ""
+        }`}
+      >
+        Automatic transition
+        <br />4 plugs
+        <br />
+        first Body paint
+        <br />
+        25 litres tank
+        <br />
+        Just like New
+        <br />
+        Buy and Drive
+      </div>
+      <div
+        className={`${classes.unsplashZbZ_EJJ4To} ${
+          props.classes?.unsplashZbZ_EJJ4To || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle200} ${
+          props.classes?.rectangle200 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle201} ${
+          props.classes?.rectangle201 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle202} ${
+          props.classes?.rectangle202 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle204} ${
+          props.classes?.rectangle204 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle203} ${
+          props.classes?.rectangle203 || ""
+        }`}
+      ></div>
+      <Ellipse26Icon
+        className={`${classes.ellipse26} ${props.classes?.ellipse26 || ""}`}
+      />
+      <div
+        className={`${classes.certifiedCarDealer} ${
+          props.classes?.certifiedCarDealer || ""
+        }`}
+      >
+        Certified Car Dealer
+      </div>
+      <div className={`${classes.lagosNG} ${props.classes?.lagosNG || ""}`}>
+        Lagos NG
+      </div>
+      <div
+        className={`${classes.seunOlulade} ${props.classes?.seunOlulade || ""}`}
+      >
+        Seun Olulade
+      </div>
+      <div
+        className={`${classes.rectangle2042} ${
+          props.classes?.rectangle2042 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.viewProfile} ${props.classes?.viewProfile || ""}`}
+      >
+        View profile
+      </div>
+      <div
+        className={`${classes.similarInOutlet} ${
+          props.classes?.similarInOutlet || ""
+        }`}
+      >
+        Similar in outlet
+      </div>
+      <div className={`${classes.frame11} ${props.classes?.frame11 || ""}`}>
+        <div
+          className={`${classes.unsplashOUBjd22gF6w} ${
+            props.classes?.unsplashOUBjd22gF6w || ""
+          }`}
+        ></div>
+        <div
+          className={`${classes.unsplashP7tai9P7HS} ${
+            props.classes?.unsplashP7tai9P7HS || ""
+          }`}
+        ></div>
+        <div
+          className={`${classes.unsplashO6yH_yAc2Ws} ${
+            props.classes?.unsplashO6yH_yAc2Ws || ""
+          }`}
+        ></div>
+        <div
+          className={`${classes.toyotaCamry72} ${
+            props.classes?.toyotaCamry72 || ""
+          }`}
+        >
+          Toyota Camry 07
+        </div>
+        <div
+          className={`${classes.washingMachine} ${
+            props.classes?.washingMachine || ""
+          }`}
+        >
+          Washing Machine
+        </div>
+        <div
+          className={`${classes._2BedroomFlat} ${
+            props.classes?._2BedroomFlat || ""
+          }`}
+        >
+          2 bedroom flat
+        </div>
+        <div className={`${classes._200002} ${props.classes?._200002 || ""}`}>
+          #2,000,000
+        </div>
+        <div className={`${classes._5000} ${props.classes?._5000 || ""}`}>
+          #50,000
+        </div>
+        <div
+          className={`${classes._30000PerAnnum} ${
+            props.classes?._30000PerAnnum || ""
+          }`}
+        >
+          #300,000 per annum
+        </div>
+        <div
+          className={`${classes.lagosNigeria} ${
+            props.classes?.lagosNigeria || ""
+          }`}
+        >
+          Lagos, Nigeria
+        </div>
+        <div
+          className={`${classes.abujaNigeria} ${
+            props.classes?.abujaNigeria || ""
+          }`}
+        >
+          Abuja, Nigeria
+        </div>
+        <div
+          className={`${classes.alakiaIbadan} ${
+            props.classes?.alakiaIbadan || ""
+          }`}
+        >
+          Alakia, Ibadan
+        </div>
+      </div>
+      <div
+        className={`${classes.images1} ${props.classes?.images1 || ""}`}
+      ></div>
+      <div
+        className={`${classes.rectangle208} ${
+          props.classes?.rectangle208 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes._2GEDA11} ${props.classes?._2GEDA11 || ""}`}
+      ></div>
+      <div
+        className={`${classes.rectangle199} ${
+          props.classes?.rectangle199 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.rectangle195} ${
+          props.classes?.rectangle195 || ""
+        }`}
+      ></div>
+      <div
+        className={`${classes.messageSeller} ${
+          props.classes?.messageSeller || ""
+        }`}
+      >
+        Message Seller
+      </div>
+      <div className={`${classes.details} ${props.classes?.details || ""}`}>
+        Details
+      </div>
+      <Ellipse14Icon
+        className={`${classes.ellipse14} ${props.classes?.ellipse14 || ""}`}
+      />
+      <ArrowLeftIcon
+        className={`${classes.arrowLeft} ${props.classes?.arrowLeft || ""}`}
+      />
+    </div>
+  );
+});
